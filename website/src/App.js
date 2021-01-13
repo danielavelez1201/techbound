ce931b1fcf01284bef0e41264093d2ff47440f2b
Jordan Lawanson <jlawanson@college.harvard.edu>
2021-01-13 17:37:47 -0500

    debugging

--- website/src/App.js
+++ website/src/App.js
@@ -32,7 +32,7 @@ function App() {
         <Header2 />
         <div className="container">
           <Switch>
-            <Route path="/" exact component={Landing} />
+            {/* <Route path="/" exact component={Landing} /> */}
             <Route path="/sign-up" exact component={SignUp} />
             <Route path="/file-save" exact component={FileSave} />
             <Route path="/file" exact component={FileUpload} />
